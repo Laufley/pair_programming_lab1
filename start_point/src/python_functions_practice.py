e1998e7cb4a7d6568f3d66ec31a5d5a84dbf20df
Indira Borras <borrasindira@gmail.com>
2022-11-24 15:55:18 +0000

    pair programming lab_1

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,56 @@
+
+def return_10():
+    return 10
+
+def add(first_number, second_number):
+    add_result = first_number + second_number
+    return add_result
+
+def subtract(number_1, number_2):
+    subtract_result = number_1 - number_2
+    return subtract_result
+
+def multiply(number_1, number_2):
+    multiply_result = number_1 * number_2
+    return multiply_result
+
+def divide(number_1, number_2):
+    divide_result = number_1 / number_2
+    return divide_result
+
+def length_of_string(string_1):
+    string_length = len(string_1)
+    total_length = string_length
+    return total_length
+
+def join_string(sentence_1, sentence_2):
+    join_string = sentence_1 + sentence_2
+    return join_string
+
+
+def add_string_as_number(string_1, string_2):
+    string_converter_1= int(string_1)
+    string_converter_2 = int(string_2) 
+    sum_of_strings = string_converter_1 + string_converter_2
+    return sum_of_strings
+    
+
+def number_to_full_month_name(month_number):
+    months_of_the_year = {1 : "January", 2 : "February", 3 : "March", 4 : "April", 5 : "May", 6 : "June", 7 : "July", 8 : "August", 9 : "September", 10 : "October", 11 : "November", 12 : "December"}
+    month = months_of_the_year[month_number]
+    return month
+
+def number_to_short_month_name(month_name):
+    months_of_the_year = {1 : "Jan", 2 : "Feb", 3 : "Mar", 4 : "Apr", 5 : "May", 6 : "Jun", 7 : "Jul", 8 : "Aug", 9 : "Sep", 10 : "Oct", 11 : "Nov", 12 : "December"}
+    month = months_of_the_year[month_name]
+    return month
+
+
+
+def volume_of_cube(sides):
+    sides_of_cube = sides * sides * sides
+    result = sides_of_cube
+    return result
+
+def 
+
